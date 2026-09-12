ac20ce69f4650e3502ccab8cb8d6d56d20915f0c
vinay10110 <vinaychakravarthi10110@gmail.com>
2026-09-12 15:43:03 +0530

    Update Terraform config for EKS 1.30 and AL2023

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -197,7 +197,7 @@ resource "aws_eks_node_group" "main" {
   version         = aws_eks_cluster.main.version
   node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = [var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id]
-  release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
+  ami_type        = "AL2023_x86_64_STANDARD"
   instance_types  = ["t3.small"]
 
   scaling_config {
--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "1.3.9"
+  required_version = ">= 1.3.9"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.55.0"
+      version = ">= 5.0"
     }
   }
 }
